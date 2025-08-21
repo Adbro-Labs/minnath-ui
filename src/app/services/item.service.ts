13e2d14332ad869dcea05dc3098c6eef930d78a1
Hamras <khamras@gmail.com>
2025-08-21 07:58:50 +0530

    serice url update

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Item, ItemVariant } from "../models/item";
-import { environment } from '../../environments/environment.development';
+import { Item } from "../models/item";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    baseUrl: "https://api.greenshoreqa.com"
+    baseUrl: "https://minnathapi.adbro.in"
 };
